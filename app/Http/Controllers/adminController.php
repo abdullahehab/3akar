d047c47d80a2418715cb7cf7bf2eea5b18f76a1b
abdullahehab <nrtroz.ae@gmail.com>
2018-05-24 19:08:46 +0200

    create index function

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class adminController extends Controller
 {
-    //
+    //index function to run base layout view of admin panel
+    public function index(){
+        return view('admin.layouts.layouts');
+    }
 }
