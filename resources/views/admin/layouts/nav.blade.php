4f0dc9c966644c4ebedbb784c959814f9d2b84ab
abdullahehab <nrtroz.ae@gmail.com>
2018-06-04 20:15:46 +0200

    create user nav list

--- resources/views/admin/layouts/nav.blade.php
+++ resources/views/admin/layouts/nav.blade.php
@@ -41,6 +41,7 @@
 
 
 {{--- User nav bar --}}
+
 <!-- Sidebar Menu -->
 <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
@@ -50,29 +51,24 @@
             <a href="#" class="nav-link active">
                 <i class="nav-icon fa fa-dashboard"></i>
                 <p>
-                    Dashboard
+                    Users Control
                     <i class="right fa fa-angle-left"></i>
                 </p>
             </a>
             <ul class="nav nav-treeview">
                 <li class="nav-item">
-                    <a href="./index.html" class="nav-link active">
+                    <a href="{{url('/adminpanel/users')}}" class="nav-link active">
                         <i class="fa fa-circle-o nav-icon"></i>
-                        <p>Dashboard v1</p>
+                        <p>View all users</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="./index2.html" class="nav-link">
+                    <a href="{{url('/adminpanel/users/create')}}" class="nav-link">
                         <i class="fa fa-circle-o nav-icon"></i>
-                        <p>Dashboard v2</p>
+                        <p>Add new user</p>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a href="./index3.html" class="nav-link">
-                        <i class="fa fa-circle-o nav-icon"></i>
-                        <p>Dashboard v3</p>
-                    </a>
-                </li>
+
             </ul>
         </li>
 
