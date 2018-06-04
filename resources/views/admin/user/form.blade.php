84cb08132fab28ee4c9193672df239bd26e7a17e
abdullahehab <nrtroz.ae@gmail.com>
2018-06-05 01:56:17 +0200

    edit input filed to laravel Form::text

--- resources/views/admin/user/form.blade.php
+++ resources/views/admin/user/form.blade.php
@@ -5,7 +5,7 @@
                             <label for="name" class="col-md-3 control-label">Name</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                {!! Form::text('name' , null , ['class' => "form-control"]) !!}
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
@@ -19,7 +19,8 @@
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                {!! Form::text('email' , null , ['class' => "form-control"]) !!}
+
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -28,6 +29,21 @@
                                 @endif
                             </div>
                         </div>
+
+                        <div class="form-group{{ $errors->has('admin') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-3 control-label">Role</label>
+
+                            <div class="col-md-6">
+                                {!! Form::select('admin' , ['0' => 'user' , '1' => 'admin'] , null , ['class' => "form-control"]) !!}
+
+                                @if ($errors->has('admin'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('admin') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 control-label">Password</label>
@@ -54,7 +70,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                    OK
                                 </button>
                             </div>
                         </div>
