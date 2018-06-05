48698f1564d6724234061df8040cfc2f562ab233
abdullahehab <nrtroz.ae@gmail.com>
2018-06-05 02:35:30 +0200

    Change password section

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -61,6 +61,59 @@
         </div>
     </section>
 
+    {{--Change password section --}}
+    <section class="content">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Change Password</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+
+                        {!! Form::open(['url' => url('adminpanel/user/changepassword/') ,'method' =>'post']) !!}
+
+                        <input type="hidden" value="{{$userEdit->id}}">
+
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <label for="password" class="col-md-4 control-label">Password</label>
+
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" name="password" required>
+
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+
+                                <div class="col-md-6">
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                </div>
+                            </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    change password
+                                </button>
+                            </div>
+                        </div>
+                        {!! Form::close() !!}
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
 @endsection
 
 @section("footer")
