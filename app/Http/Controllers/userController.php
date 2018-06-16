fa649237971f936d0c1452373018a7bc3db6cf3a
abdullahehab <nrtroz.ae@gmail.com>
2018-06-16 06:49:10 +0200

    remove compact method from index function

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -10,8 +10,8 @@ class userController extends Controller
 
     public function index()
     {
-        $users = User::all();
-        return view('admin.user.index', compact('users'));
+        
+        return view('admin.user.index');
 
     }
 
