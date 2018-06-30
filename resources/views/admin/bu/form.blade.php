2a1d576fb8019664570714e4e34bf7ec8a33c5ec
root <220150134@fa-hists.edu.eg>
2018-07-01 00:20:53 +0200

    build form code of bu

--- resources/views/admin/bu/form.blade.php
+++ resources/views/admin/bu/form.blade.php
@@ -1,69 +1,174 @@
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                        <div class="form-group{{ $errors->has('bu_name') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-3 control-label">Name</label>
 
                             <div class="col-md-6">
-                                {!! Form::text('name' , null , ['class' => "form-control"]) !!}
+                                {!! Form::text('bu_name' , null , ['class' => "form-control"]) !!}
 
-                                @if ($errors->has('name'))
+                                @if ($errors->has('bu_name'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
+                                        <strong>{{ $errors->first('bu_name') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                        <div class="form-group{{ $errors->has('bu_rooms') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-3 control-label">Rooms Number</label>
 
                             <div class="col-md-6">
-                                {!! Form::text('email' , null , ['class' => "form-control"]) !!}
+                                {!! Form::text('bu_rooms' , null , ['class' => "form-control"]) !!}
 
-
-                                @if ($errors->has('email'))
+                                @if ($errors->has('bu_rooms'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong>{{ $errors->first('bu_rooms') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('admin') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-3 control-label">Role</label>
+                        <div class="form-group{{ $errors->has('bu_price') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-3 control-label">Price</label>
 
                             <div class="col-md-6">
-                                {!! Form::select('admin' , ['0' => 'user' , '1' => 'admin'] , null , ['class' => "form-control"]) !!}
+                                {!! Form::text('bu_price' , null , ['class' => "form-control"]) !!}
 
-                                @if ($errors->has('admin'))
+                                @if ($errors->has('bu_price'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('admin') }}</strong>
+                                        <strong>{{ $errors->first('bu_price') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        @if(!isset($userEdit))
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                        <div class="form-group{{ $errors->has('bu_rent') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-3 control-label">Process type</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                {!! Form::select('bu_rent' , buRent(), null , ['class' => "form-control"]) !!}
 
-                                @if ($errors->has('password'))
+                                @if ($errors->has('bu_rent'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
+                                        <strong>{{ $errors->first('bu_rent') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                        <div class="form-group{{ $errors->has('bu_square') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-3 control-label">Square</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                {!! Form::text('bu_square' , null , ['class' => "form-control"]) !!}
+
+                                @if ($errors->has('bu_square'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('bu_square') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
-                        @endif
+
+                        <div class="form-group{{ $errors->has('bu_type') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-3 control-label">Type</label>
+
+                            <div class="col-md-6">
+                                {!! Form::select('bu_type' , buType() , null , ['class' => "form-control"]) !!}
+
+                                @if ($errors->has('bu_type'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('bu_type') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('bu_status') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-3 control-label">Status</label>
+
+                            <div class="col-md-6">
+                                {!! Form::select('bu_status' , buStatus() , null , ['class' => "form-control"]) !!}
+
+                                @if ($errors->has('bu_status'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('bu_status') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('bu_meta') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-3 control-label">Keywords</label>
+
+                            <div class="col-md-6">
+                                {!! Form::text('bu_meta' , null , ['class' => "form-control"]) !!}
+
+                                @if ($errors->has('bu_meta'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('bu_meta') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('bu_small_des') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-3 control-label">Small Description for search engine</label>
+
+                            <div class="col-md-6">
+                                {!! Form::textarea('bu_small_des' , null , ['class' => "form-control", 'rows' => '4']) !!}
+
+                                @if ($errors->has('bu_small_des'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('bu_small_des') }}</strong>
+                                    </span>
+                                @endif
+                                <br>
+                                <div class="alert alert-warning">
+                                    No more than 160 characters are allowed .
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('bu_langtuide') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-3 control-label">Longitude</label>
+
+                            <div class="col-md-6">
+                                {!! Form::text('bu_langtuide' , null , ['class' => "form-control"]) !!}
+
+                                @if ($errors->has('bu_langtuide'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('bu_langtuide') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('bu_latitude') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-3 control-label">Latitude</label>
+
+                            <div class="col-md-6">
+                                {!! Form::text('bu_latitude' , null , ['class' => "form-control"]) !!}
+
+                                @if ($errors->has('bu_latitude'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('bu_latitude') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('bu_large_dis') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-3 control-label">Long Description</label>
+
+                            <div class="col-md-6">
+                                {!! Form::textarea('bu_large_dis' , null , ['class' => "form-control"]) !!}
+
+                                @if ($errors->has('bu_large_dis'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('bu_large_dis') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
