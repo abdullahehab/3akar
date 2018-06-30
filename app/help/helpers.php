396bea170250f8a71bf834acba0bcbce9aa1ed5d
root <220150134@fa-hists.edu.eg>
2018-06-30 23:15:19 +0200

    add buType function

--- app/help/helpers.php
+++ app/help/helpers.php
@@ -5,3 +5,11 @@ function getSetting($settingName = 'name'){ // by default $sittingName equal nam
 
     return \App\SiteSetting::where('namesetting', $settingName)->get()[0]->value;
 }
+
+// Return array contain tupe of builds
+function buType(){
+    $array = [
+      'شقه', 'فيلا', 'شاليه'
+    ];
+    return $array;
+}
