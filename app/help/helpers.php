48b916f356c644fa98321079b246c5592bc070b1
root <220150134@fa-hists.edu.eg>
2018-06-30 22:43:15 +0200

    build function bodu

--- app/help/helpers.php
+++ app/help/helpers.php
@@ -1,5 +1,7 @@
 <?php
+
 //function to return to values of SiteSetting table
 function getSetting($settingName = 'name'){ // by default $sittingName equal name of namesetting col.
+
     return \App\SiteSetting::where('namesetting', $settingName)->get()[0]->value;
 }
