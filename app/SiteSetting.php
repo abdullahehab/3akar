4ce56d673ad42640b0069c3b1f5f955db4bae8a3
abdullah-ehab <220150134@fa-hists.edu.eg>
2018-06-30 04:49:58 +0200

    edit table name

--- app/SiteSetting.php
+++ app/SiteSetting.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class SiteSetting extends Model
 {
-    protected $table = 'sitesetting';
+    protected $table = 'siteSetting';
 
     
     protected $fillable = [
