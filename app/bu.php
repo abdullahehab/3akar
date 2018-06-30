befefafd36e2d7c14a35942ad589cc371747d6a0
root <220150134@fa-hists.edu.eg>
2018-06-30 22:46:54 +0200

    pass table name & fillable

--- app/bu.php
+++ app/bu.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class bu extends Model
 {
-    //
+    protected $table = "bu"; // pass table name
+
+    protected $fillable = [
+        'bu_name', 'bu_price', 'bu_rent', 'bu_square', 'bu_type', 'bu_small_des', 'bu_meta', 'bu_langtuide', 'bu_latitude', 'bu_large_dis', 'bu_status',  'user_id'
+
+    ];
 }
