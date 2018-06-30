cadf439a9ce959f80cc1e6f0a9edf60b028e3255
root <220150134@fa-hists.edu.eg>
2018-06-30 23:49:35 +0200

    add bu_rooms col

--- app/bu.php
+++ app/bu.php
@@ -9,7 +9,7 @@ class bu extends Model
     protected $table = "bu"; // pass table name
 
     protected $fillable = [
-        'bu_name', 'bu_price', 'bu_rent', 'bu_square', 'bu_type', 'bu_small_des', 'bu_meta', 'bu_langtuide', 'bu_latitude', 'bu_large_dis', 'bu_status',  'user_id'
+        'bu_name', 'bu_price', 'bu_rent', 'bu_square', 'bu_type', 'bu_small_des', 'bu_meta', 'bu_langtuide', 'bu_latitude', 'bu_large_dis', 'bu_status',  'user_id', 'bu_rooms'
 
     ];
 }
