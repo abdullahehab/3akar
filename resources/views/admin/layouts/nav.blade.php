829cb726364d484edb4fc0834d2e3f421bb7e1df
root <220150134@fa-hists.edu.eg>
2018-06-30 23:24:01 +0200

    create Immovables nav bar

--- resources/views/admin/layouts/nav.blade.php
+++ resources/views/admin/layouts/nav.blade.php
@@ -75,3 +75,43 @@
     </ul>
 </nav>
 <!-- /.sidebar-menu -->
+
+
+
+
+
+{{--- bu nav bar --}}
+
+<!-- Sidebar Menu -->
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
+             with font-awesome or any other icon font library -->
+        <li class="nav-item has-treeview">
+            <a href="#" class="nav-link active">
+                <i class="nav-icon fa fa-dashboard"></i>
+                <p>
+                    Immovables Control
+                    <i class="right fa fa-angle-left"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="{{url('/adminpanel/bu')}}" class="nav-link active">
+                        <i class="fa fa-circle-o nav-icon"></i>
+                        <p>View all Immovables</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{url('/adminpanel/bu/create')}}" class="nav-link">
+                        <i class="fa fa-circle-o nav-icon"></i>
+                        <p>Add new Immovables</p>
+                    </a>
+                </li>
+
+            </ul>
+        </li>
+
+    </ul>
+</nav>
+<!-- /.sidebar-menu -->
