b1683b94e9d30537fdd561d1642e61c7e2ede48e
root <220150134@fa-hists.edu.eg>
2018-06-30 05:30:27 +0200

    build getSetting function

---
+++ app/help/helpers.php
@@ -0,0 +1,5 @@
+<?php
+//function to return to values of SiteSetting table
+function getSetting($settingName = 'name'){ // by default $sittingName equal name of namesetting col.
+    return \App\SiteSetting::where('namesetting', $settingName)->get()[0]->value;
+}
