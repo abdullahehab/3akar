9718cf3bc65e0e8fffd7ff1e2d655d66744e4425
root <220150134@fa-hists.edu.eg>
2018-06-30 22:57:50 +0200

    bu main files

---
+++ resources/views/admin/bu/edit.blade.php
@@ -0,0 +1,114 @@
+@extends('admin.layouts.layouts')
+
+@section('title')
+
+    Edit
+    {{$userEdit->name}}
+     Info
+
+@endsection
+
+
+@section("header")
+
+
+@endsection
+
+
+@section('content')
+
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Edit {{$userEdit->name}} Info</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{url ('/adminpanel')}}">Home</a></li>
+                        <li class="breadcrumb-item"><a href="{{url ('/adminpanel/users')}}">Users Control</a>
+                        <li class="breadcrumb-item active"><a href="{{url ('/adminpanel/users/'.$userEdit->id.'/edit')}}">
+                                Edit
+                                {{$userEdit->name}}
+                                Info
+                            </a>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Edit Form</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+
+                        {!! Form::model($userEdit ,['route' => ['users.update' , $userEdit->id] ,'method' =>'PATCH']) !!}
+                            @include('admin.user.form')
+                        {!! Form::close() !!}
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    {{--Change password section --}}
+    <section class="content">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Change Password</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+
+                        {!! Form::open(['url' => url('adminpanel/user/changepassword/') ,'method' =>'post']) !!}
+
+                        <input type="hidden" value="{{$userEdit->id}}" name="userID">
+
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <label for="password" class="col-md-4 control-label">Password</label>
+
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" name="password" required>
+
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    change password
+                                </button>
+                            </div>
+                        </div>
+                        {!! Form::close() !!}
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+@endsection
+
+@section("footer")
+
+
+@endsection
---
+++ resources/views/admin/bu/index.blade.php
@@ -0,0 +1,177 @@
+@extends('admin.layouts.layouts')
+
+@section('title')
+
+    Immovables Control
+
+@endsection
+
+
+@section("header")
+
+    <!-- DataTables -->
+    {{Html::style('admin/plugins/datatables/dataTables.bootstrap4.css')}}
+
+@endsection
+
+
+
+
+
+
+@section('content')
+
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Immovables table</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{url ('/adminpanel')}}">Home</a></li>
+                        <li class="breadcrumb-item active"><a href="{{url ('/adminpanel/users')}}">Immovables Control</a>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Hover Data Table</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table id="example2" class="table table-bordered table-hover">
+                            <thead>
+                            <tr>
+                                <th>Immovables Name</th>
+                                <th>Price</th>
+                                <th>Type</th>
+                                <th>created_at</th>
+                                <th>Status</th>
+                                <th>Options</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+
+                           {{--
+                           @foreach($users as $userInfo)
+                            <tr>
+                                <td>{{$userInfo->id}}</td>
+                                <td>{{$userInfo->name}}</td>
+                                <td>{{$userInfo->email}}</td>
+                                <td>{{$userInfo->created_at}}</td>
+                                <td>{{$userInfo->updated_at}}</td>
+                                <td>{{$userInfo->admin == 1 ? "admin" : "user"}}</td>
+                                <td>
+                                    <a href="{{url('adminpanel/users/'.$userInfo->id.'/edit')}}" class="btn btn-info" role="button">Edit</a>
+                                    <a href="{{url('adminpanel/users/'.$userInfo->id.'/delete')}}" class="btn btn-danger" role="button">Delete</a>
+                                </td>
+
+                            </tr>
+                            @endforeach
+                           --}}
+
+                            </tbody>
+                            <tfoot>
+                            <tr>
+                                <th>Immovables Name</th>
+                                <th>Price</th>
+                                <th>Type</th>
+                                <th>created_at</th>
+                                <th>Status</th>
+                                <th>Options</th>
+                            </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+                
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </section>
+
+@endsection
+
+
+
+
+
+
+@section('footer')
+
+    <!-- DataTables -->
+    {{Html::script('admin/plugins/datatables/jquery.dataTables.min.js')}}
+    {{Html::script('admin/plugins/datatables/dataTables.bootstrap4.min.js')}}
+
+
+    <script>
+
+        var table = $('#example2').DataTable({
+           processing: true,
+           serverSide: true,
+           ajax: "/adminpanel/users/data",
+            columns:[
+                {data: 'id',name: 'id'},
+                {data: 'name',name: 'name'},
+                {data: 'email',name: 'email'},
+                {data: 'created_at',name: 'created_at'},
+                {data: 'updated_at',name: 'updated_at'},
+                {data: 'admin',name: 'admin'},
+                {data: 'control',name: ''}
+
+            ],
+
+            "stateSave": false,
+            'responsive': true,
+            'order': [[0, 'desc']],
+            'pagingType': "full_numbers",
+            'aLengthMenu': [
+                [20, 50, 100, 200, -1],
+                [20, 50, 100, 200, "All"]
+            ],
+            iDisplayLength: 25,
+            fixedHeader: true,
+
+            'oTableTools': {
+               'aButtons': [
+
+                   {
+                       'eExtends': 'csv',
+                       'sButtonText': 'Excel File',
+                       'sCharSet': 'utf16le'
+                   },
+                   {
+                       'eExtends': 'copy',
+                       'sButtonText': 'Copy information'
+                   },
+                   {
+                       'eExtends': 'print',
+                       'sButtonText': 'print',
+                       'sCharSet': 'visible'
+                   }
+
+               ],
+
+                "sSwfPath": "{{Request::root()}}}/admin/customize/copy_csv_xls.swf"
+
+
+        }
+
+        });
+
+
+    </script>
+
+@endsection
