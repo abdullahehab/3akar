162331ce1f36ff9054ff8e1dadb4e720e8a74731
root <220150134@fa-hists.edu.eg>
2018-06-30 23:15:55 +0200

    build andData function

--- app/Http/Controllers/buController.php
+++ app/Http/Controllers/buController.php
@@ -2,11 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\bu;
 use Illuminate\Http\Request;
 
 class buController extends Controller
 {
     public function index(){
         return view('admin.bu.index');
+    }
+
+
+    public function anyData()
+    {
+
+        return DataTables::of(bu::all())
+            ->editColumn('bu_name', function ($model){
+                return $model->name;
+            })
+
+            ->editColumn('bu_status', function ($model) {
+                return $model->bu_status == 0 ? 'مٌفعل' : 'غير مٌفعل';
+
+            })
+
+            ->editColumn('bu_type', function ($model) {
+                $type = buType();
+                return $type[$model->type];
+
+            })
+
+            ->editColumn('control', function ($model) {
+
+                $all = '<a href="'.url('/adminpanel/bu/'.$model->id.'/edit').'" class="btn btn-info btn-default"><i class="fa fa-edit"></i></a>';
+                $all .= '<a  href="'.url('/adminpanel/bu/'.$model->id.'/delete').'" class="btn btn-danger"><i class="fa fa-trash-o"></i></a>';
+                return $all;
+            })
+            ->make(true);
     }
 }
