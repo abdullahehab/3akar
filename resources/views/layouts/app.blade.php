21eadf38b1cffc73dddfc9816f6e1c3a3aed7db9
root <220150134@fa-hists.edu.eg>
2018-06-30 05:38:42 +0200

    call getSetting funciton

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,7 +74,11 @@
 
     <div class="footer">
         <div class="footer_bottom">
-            <div class="follow-us"> <a class="fa fa-facebook social-icon" href="#"></a> <a class="fa fa-twitter social-icon" href="#"></a> <a class="fa fa-linkedin social-icon" href="#"></a> <a class="fa fa-google-plus social-icon" href="#"></a> </div>
+            <div class="follow-us">
+                <a class="fa fa-facebook social-icon" href="{{getSetting('facebook')}}"></a>
+                <a class="fa fa-twitter social-icon" href="{{getSetting('twitter')}}"></a>
+                <a class="fa fa-linkedin social-icon" href="#"></a>
+                <a class="fa fa-youtube social-icon" href="{{getSetting('youtube')}}"></a> </div>
             <div class="copy">
                 <p>Copyright &copy; 2015 Company Name. Design by <a href="http://www.templategarden.com" rel="nofollow">TemplateGarden</a></p>
             </div>
