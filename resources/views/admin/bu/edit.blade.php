5883f5425ae490394ae26d6d754ec47f547816a3
root <220150134@fa-hists.edu.eg>
2018-07-01 07:30:41 +0200

    edit page edit.blade.php

--- resources/views/admin/bu/edit.blade.php
+++ resources/views/admin/bu/edit.blade.php
@@ -3,7 +3,7 @@
 @section('title')
 
     Edit
-    {{$userEdit->name}}
+    {{$bu->bu_name}}
      Info
 
 @endsection
@@ -21,15 +21,15 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Edit {{$userEdit->name}} Info</h1>
+                    <h1>Edit {{$bu->bu_name}} Info</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="{{url ('/adminpanel')}}">Home</a></li>
-                        <li class="breadcrumb-item"><a href="{{url ('/adminpanel/users')}}">Users Control</a>
-                        <li class="breadcrumb-item active"><a href="{{url ('/adminpanel/users/'.$userEdit->id.'/edit')}}">
+                        <li class="breadcrumb-item"><a href="{{url ('/adminpanel/bu')}}">Immovables Control</a>
+                        <li class="breadcrumb-item active"><a href="{{url ('/adminpanel/bu/'.$bu->id.'/edit')}}">
                                 Edit
-                                {{$userEdit->name}}
+                                {{$bu->bu_name}}
                                 Info
                             </a>
                     </ol>
@@ -50,8 +50,8 @@
                     <!-- /.card-header -->
                     <div class="card-body">
 
-                        {!! Form::model($userEdit ,['route' => ['users.update' , $userEdit->id] ,'method' =>'PATCH']) !!}
-                            @include('admin.user.form')
+                        {!! Form::model($bu ,['route' => ['bu.update' , $bu->id] ,'method' =>'PATCH']) !!}
+                            @include('admin.bu.form')
                         {!! Form::close() !!}
 
 
@@ -61,50 +61,7 @@
         </div>
     </section>
 
-    {{--Change password section --}}
-    <section class="content">
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Change Password</h3>
-                    </div>
-                    <!-- /.card-header -->
-                    <div class="card-body">
 
-                        {!! Form::open(['url' => url('adminpanel/user/changepassword/') ,'method' =>'post']) !!}
-
-                        <input type="hidden" value="{{$userEdit->id}}" name="userID">
-
-                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label for="password" class="col-md-4 control-label">Password</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="password" required>
-
-                                    @if ($errors->has('password'))
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('password') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    change password
-                                </button>
-                            </div>
-                        </div>
-                        {!! Form::close() !!}
-
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
 
 @endsection
 
