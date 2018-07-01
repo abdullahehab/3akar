380508ecbf8243fdc70b789c4335b5cb4aa427ec
root <220150134@fa-hists.edu.eg>
2018-07-01 05:37:18 +0200

    build store function

--- app/Http/Controllers/buController.php
+++ app/Http/Controllers/buController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\bu;
+use App\Http\Requests\buRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class buController extends Controller
 {
@@ -13,6 +16,26 @@ class buController extends Controller
 
     public function create(){
         return view('admin.bu.add');
+    }
+
+    public function store(buRequest $burequest, bu $bu){
+        $data = [
+            'bu_name'       =>  $burequest  ->  bu_name,
+            'bu_price'      =>  $burequest  ->  bu_price,
+            'bu_rent'       =>  $burequest  ->  bu_rent,
+            'bu_square'     =>  $burequest  ->  bu_square,
+            'bu_type'       =>  $burequest  ->  bu_type,
+            'bu_small_des'  =>  $burequest  ->  bu_small_des,
+            'bu_meta'       =>  $burequest  ->  bu_meta,
+            'bu_langtuide'  =>  $burequest  -> bu_langtuide,
+            'bu_latitude'   =>  $burequest  ->  bu_latitude,
+            'bu_large_dis'  =>  $burequest  ->  bu_large_dis,
+            'bu_status'     =>  $burequest  ->  bu_status,
+            'user_id'       => Auth::id(),
+            'bu_rooms'      => $burequest   -> bu_rooms,
+        ];
+        $bu->create($data);
+        return redirect('adminpanel/bu');
     }
 
 
