c231f564831b7bdfa4d0b19a89fd61269f5ec4d7
root <220150134@fa-hists.edu.eg>
2018-07-03 07:27:48 +0200

    edit vaule of foreach

--- resources/views/admin/website/bu/shareFile.blade.php
+++ resources/views/admin/website/bu/shareFile.blade.php
@@ -5,11 +5,11 @@
             <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
                 <div class="my-list">
                     <img src="http://hpservicecenterschennai.in/images/hp_laptop_service_centers_in_guindy.png" alt="dsadas" />
-                    <h3>{{$bu->bu_name}}</h3>
-                    <span>RS:45K</span>
-                    <span class="pull-right">{{$bu->bu_price}}</span>
+                    <h3>{{$b->bu_name}}</h3>
+                    <span>Month Added : {{$b->created_at->month}}</span>
+                    <span class="pull-right">{{$b->bu_price}}</span>
                     <div class="detail">
-                        <p>{{str_limit($bu->bu_small_des, 65)}}</p>
+                        <p>{{str_limit($b->bu_small_des, 65)}}</p>
                         <img src="http://hpservicecenterschennai.in/images/hp_laptop_service_centers_in_guindy.png" alt="dsadas" />
                         <a href="#" class="btn btn-info">Add To Cart</a>
                         <a href="#" class="btn btn-info">Detail</a>
