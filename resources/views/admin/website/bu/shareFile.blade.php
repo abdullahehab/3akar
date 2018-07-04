4da315c764d7716dde9da64a2695ceed99e8a18d
root <220150134@fa-hists.edu.eg>
2018-07-04 05:08:30 +0200

    devide array to 4 item

--- resources/views/admin/website/bu/shareFile.blade.php
+++ resources/views/admin/website/bu/shareFile.blade.php
@@ -1,20 +1,25 @@
 @if(count($bu) > 0)
 
-    @foreach($bu as $b)
-            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                <div class="my-list">
-                    <img src="http://hpservicecenterschennai.in/images/hp_laptop_service_centers_in_guindy.png" alt="dsadas" />
-                    <h3>{{$b->bu_name}}</h3>
-                    <span>Month Added : {{$b->created_at->month}}</span>
-                    <span class="pull-right">{{$b->bu_price}}</span>
-                    <div class="detail">
-                        <p>{{str_limit($b->bu_name, 65)}}</p>
-                        <img src="http://hpservicecenterschennai.in/images/hp_laptop_service_centers_in_guindy.png" alt="dsadas" />
-                        <a href="#" class="btn btn-info">Add To Cart</a>
-                        <a href="#" class="btn btn-info">Detail</a>
+    {{-- Devide array to 4 items for each row --}}
+    @foreach(array_chunk($bu, 4) as $buu)
+        <div class="row">
+            @foreach($buu as $b)
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                        <div class="my-list">
+                            <img src="http://hpservicecenterschennai.in/images/hp_laptop_service_centers_in_guindy.png" alt="dsadas" />
+                            <h3>{{$b['bu_name']}}</h3>
+                            <span>Month Added : {{$b['created_at'][6]}}</span> {{-- 6 is the index of month in array --}}
+                            <span class="pull-right">{{$b['bu_price']}}</span>
+                            <div class="detail">
+                                <p>{{str_limit($b['bu_name'], 65)}}</p>
+                                <img src="http://hpservicecenterschennai.in/images/hp_laptop_service_centers_in_guindy.png" alt="dsadas" />
+                                <a href="#" class="btn btn-info">Add To Cart</a>
+                                <a href="#" class="btn btn-info">Detail</a>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
+            @endforeach
+        </div>
     @endforeach
 
 @else
