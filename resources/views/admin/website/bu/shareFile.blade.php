26d83e586ba7206e6118e13507286cebd813513b
root <220150134@fa-hists.edu.eg>
2018-07-07 04:28:38 +0200

    replace array to paginate

--- resources/views/admin/website/bu/shareFile.blade.php
+++ resources/views/admin/website/bu/shareFile.blade.php
@@ -1,17 +1,19 @@
 @if(count($bu) > 0)
 
-    {{-- Devide array to 4 items for each row --}}
-    @foreach(array_chunk($bu, 4) as $buu)
-        <div class="row">
-            @foreach($buu as $b)
+
+            @foreach($bu as $key => $b)
+                {{-- To devide item to 4 in each row --}}
+                @if($key % 4 == 0)
+                    <div class="clearfix"></div>
+                @endif
                     <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
                         <div class="my-list">
                             <img src="http://hpservicecenterschennai.in/images/hp_laptop_service_centers_in_guindy.png" alt="dsadas" />
-                            <h3>{{$b['bu_name']}}</h3>
-                            <span>Month Added : {{$b['created_at'][6]}}</span> {{-- 6 is the index of month in array --}}
-                            <span class="pull-right">{{$b['bu_price']}}</span>
+                            <h3>{{$b->bu_name}}</h3>
+                            <span>Month: {{$b->created_at->month}}</span> {{-- 6 is the index of month in array --}}
+                            <span class="pull-right">{{$b->bu_price}}</span>
                             <div class="detail">
-                                <p>{{str_limit($b['bu_name'], 65)}}</p>
+                                <p>{{str_limit($b->bu_name, 65)}}</p>
                                 <img src="http://hpservicecenterschennai.in/images/hp_laptop_service_centers_in_guindy.png" alt="dsadas" />
                                 <a href="#" class="btn btn-info">Add To Cart</a>
                                 <a href="#" class="btn btn-info">Detail</a>
@@ -19,8 +21,6 @@
                         </div>
                     </div>
             @endforeach
-        </div>
-    @endforeach
 
 @else
     <div class="alert alert-danger">
