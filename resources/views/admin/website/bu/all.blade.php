42756de4b0d15e9127410fa13716b57d36c6862b
root <220150134@fa-hists.edu.eg>
2018-07-07 04:47:14 +0200

    edit title to website options

--- resources/views/admin/website/bu/all.blade.php
+++ resources/views/admin/website/bu/all.blade.php
@@ -18,28 +18,9 @@
         <div class="row profile">
             <div class="col-md-3">
                 <div class="profile-sidebar">
-                    <!-- SIDEBAR USERPIC -->
-                    <div class="profile-userpic">
-                        <img src="http://keenthemes.com/preview/metronic/theme/assets/admin/pages/media/profile/profile_user.jpg" class="img-responsive" alt="">
-                    </div>
-                    <!-- END SIDEBAR USERPIC -->
-                    <!-- SIDEBAR USER TITLE -->
-                    <div class="profile-usertitle">
-                        <div class="profile-usertitle-name">
-                            Marcus Doe
-                        </div>
-                        <div class="profile-usertitle-job">
-                            Developer
-                        </div>
-                    </div>
-                    <!-- END SIDEBAR USER TITLE -->
-                    <!-- SIDEBAR BUTTONS -->
-                    <div class="profile-userbuttons">
-                        <button type="button" class="btn btn-success btn-sm">Follow</button>
-                        <button type="button" class="btn btn-danger btn-sm">Message</button>
-                    </div>
-                    <!-- END SIDEBAR BUTTONS -->
-                    <!-- SIDEBAR MENU -->
+                  <h2 style="margin-left: 20px">
+                      Website Options
+                  </h2>
                     <div class="profile-usermenu">
                         <ul class="nav">
                             <li class="active">
