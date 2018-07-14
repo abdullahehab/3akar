99f442ec3117e27b0ddebd187c05ff2eec443673
abdullah ehab <220150134@fa-hists.edu.eg>
2018-07-14 17:42:28 +0200

    change table name

--- app/bu.php
+++ app/bu.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class bu extends Model
 {
-    protected $table = "bu"; // pass table name
+    protected $table = "BU"; // pass table name
 
     protected $fillable = [
         'bu_name', 'bu_price', 'bu_rent', 'bu_square', 'bu_type', 'bu_small_des', 'bu_meta', 'bu_langtuide', 'bu_latitude', 'bu_large_dis', 'bu_status',  'user_id', 'bu_rooms', 'bu_place'
