14dfb8c616a98ede883f314079c5998e66c819a6
abdullah ehab <220150134@fa-hists.edu.eg>
2018-07-17 03:14:15 +0200

    create breadcrumb

--- resources/views/admin/website/bu/all.blade.php
+++ resources/views/admin/website/bu/all.blade.php
@@ -13,6 +13,9 @@
         .itemSearch{
             margin-bottom: 10px;
         }
+        .breadcrumb{
+            background-color: white;
+        }
     </style>
 
 @endsection
@@ -118,6 +121,29 @@
 
 
             <div class="col-md-9">
+
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">Result</li>
+                    @if(isset($array))
+                        @if(!empty($array))
+                            @foreach($array as $key => $value)
+                                <li class="breadcrumb-item">
+                                    {{searchNameFiled()[$key]}} =
+                                        @if($key == 'bu_rent')
+                                            {{ buRent()[$value] }}
+                                        @elseif($key == 'bu_type')
+                                            {{ buType()[$value] }}
+                                        @elseif($key == 'bu_place')
+                                            {{ buCountry()[$value] }}
+                                        @else
+                                        {{$value}}
+                                        @endif
+                                </li>
+                            @endforeach
+                        @endif
+                     @endif
+                </ol>
+
                 <div class="profile-content">
 
                     @include('admin.website.bu.shareFile',[ 'bu' => $buAll] )
