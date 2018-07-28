9b48424652cd5aa34e2e2b6d754d530cf903df09
abdullah ehab <220150134@fa-hists.edu.eg>
2018-07-28 18:57:37 +0200

    user array_chunk method

--- resources/views/admin/website/bu/shareFile.blade.php
+++ resources/views/admin/website/bu/shareFile.blade.php
@@ -1,12 +1,74 @@
 @if(count($bu) > 0)
 
+    <div class="container">
+        @foreach(array_chunk($bu, 3) as $key)
+            <div class="row">
+                @foreach($key as $add)
+                    <div class="col-md-3">
+                        <span class="thumbnail">
+                            <img src="https://s12.postimg.org/41uq0fc4d/item_2_180x200.png" alt="...">
+                            <h4>Product Tittle</h4>
+                            <div class="ratings">
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                            <hr class="line">
+                            <div class="row">
+                                <div class="col-md-6 col-sm-6">
+                                    <p class="price">$29,90</p>
+                                </div>
+                                <div class="col-md-6 col-sm-6">
+                                 <a href="http://cookingfoodsworld.blogspot.in/" target="_blank" >	<button class="btn btn-info right" > BUY ITEM</button></a>
+                                </div>
 
-            @foreach($bu as $key => $b)
-                {{-- To devide item to 4 in each row --}}
-                @if($key % 4 == 0)
-                    <div class="clearfix"></div>
-                @endif
-                    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                            </div>
+                        </span>
+                    </div>
+                @endforeach
+            </div>
+        @endforeach
+    </div>
+            {{--@foreach($bu as $key => $b)
+                <div class="row">
+
+
+
+                </div>
+                --}}{{-- To devide item to 4 in each row --}}{{--
+
+                --}}{{--<div class="container">
+                  <div class="row">
+                      <div class="col-md-3 col-sm-6">
+                        <span class="thumbnail">
+                            <img src="https://s12.postimg.org/41uq0fc4d/item_2_180x200.png" alt="...">
+                            <h4>Product Tittle</h4>
+                            <div class="ratings">
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                            </div>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                            <hr class="line">
+                            <div class="row">
+                                <div class="col-md-6 col-sm-6">
+                                    <p class="price">$29,90</p>
+                                </div>
+                                <div class="col-md-6 col-sm-6">
+                                 <a href="http://cookingfoodsworld.blogspot.in/" target="_blank" >	<button class="btn btn-info right" > BUY ITEM</button></a>
+                                </div>
+
+                            </div>
+                        </span>
+                    </div>
+                    </div>
+                </div>--}}{{--
+              --}}{{--      <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
                         <div class="my-list">
                             <img src="http://hpservicecenterschennai.in/images/hp_laptop_service_centers_in_guindy.png" alt="dsadas" />
                             <h3>{{$b->bu_name}}</h3>
@@ -21,8 +83,8 @@
                                 <a href="{{ url('/singleBuilding/'.$b->id) }}" class="btn btn-info center-block">Detail</a>
                             </div>
                         </div>
-                    </div>
-            @endforeach
+                    </div>--}}{{--
+            @endforeach--}}
 
 @else
     <div class="alert alert-danger">
