8f9f7b79689f657e23b8721160c2a14e929615cc
abdullah ehab <220150134@fa-hists.edu.eg>
2018-08-28 06:01:28 +0200

    add properties of builds

--- resources/views/admin/website/bu/shareFile.blade.php
+++ resources/views/admin/website/bu/shareFile.blade.php
@@ -1,17 +1,39 @@
 @if(count($bu) > 0)
-            <div class="row">
-                @foreach($bu as $b)
-                    <div class="col-md-4">
-                        <div class="productbox">
-                            <img src="http://lorempixel.com/460/250/" class="img-responsive">
-                            <div class="producttitle">{{ $b->bu_name }}</div>
-                            dic
-                            <p class="text-justify">The product is the greatest product ever, it will do what it's designed for. No more, no less.</p>
-                            <div class="productprice"><div class="pull-right"><a href="#" class="btn btn-primary btm-sm" role="button">Add to <span class="glyphicon glyphicon-shopping-cart"></span></a></div><div class="pricetext">8.95 €</div></div>
-                        </div>
+    @foreach($bu as $key => $b)
+        @if($key % 3 == 0 && $key !=0)
+            <div class="clearfix"></div>
+        @endif
+        <div class="col-md-4">
+            <div class="productbox">
+                <img src="http://lorempixel.com/460/250/" class="img-responsive">
+                <div class="producttitle">{{ $b->bu_name }}</div>
+                <p class="text-justify">{{ str_limit($b->bu_small_des, 70) }}</p>
+
+                <div class="productprice ">
+
+                    <span class="pull-right">
+                        المساحه : {{ $b->bu_square }}
+                    </span>
+                    <span class="pull-left">
+                        نوع العمليه : {{ buRent()[$b->bu_rent] }}
+                    </span>
+                    <div class="clearfix"></div>
+
+                    <hr>
+                    <div class="pull-right">
+                        <a href="#" class="btn btn-primary btm-sm" role="button">إظهار التفاصيل
+                            <span class="fa fa-arrow-circle-o-right" style="color:#FFFFFF"></span>
+                        </a>
                     </div>
-                @endforeach
+                    <div class="pricetext">{{ $b->bu_price }}</div>
+                </div>
             </div>
+        </div>
+
+    @endforeach
+
+    <div class="clearfix"></div>
+    <br>
 @else
     <div class="alert alert-danger">
         There are currently no All Real Estate
