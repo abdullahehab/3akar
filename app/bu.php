a5fb10e3dd22cdc19b5e38216e819b639ece68d8
abdullah ehab <220150134@fa-hists.edu.eg>
2018-08-29 06:49:23 +0200

    add dropdown rent & sale

--- app/bu.php
+++ app/bu.php
@@ -9,7 +9,7 @@ class bu extends Model
     protected $table = "BU"; // pass table name
 
     protected $fillable = [
-        'bu_name', 'bu_price', 'bu_rent', 'bu_square', 'bu_type', 'bu_small_des', 'bu_meta', 'bu_langtuide', 'bu_latitude', 'bu_large_dis', 'bu_status',  'user_id', 'bu_rooms', 'bu_place'
+        'bu_name', 'bu_price', 'bu_rent', 'bu_square', 'bu_type', 'bu_small_des', 'bu_meta', 'bu_langtuide', 'bu_latitude', 'bu_large_dis', 'bu_status',  'user_id', 'bu_rooms', 'bu_place', 'bu_image'
 
     ];
 }
