d8111f4525221ca48d167de046f212c5908c17dd
abdullah ehab <220150134@fa-hists.edu.eg>
2018-08-31 03:27:23 +0200

    create profile view

---
+++ resources/views/admin/user/profile.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('title')
+
+    Profile page
+
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <h2 class="panel-heading">{{$user->name}} Profile</h2>
+                    <div class="panel-body">
+                        <img src="/bu_image/{{$user->avatar}}" style="width:150px; height: 150px; float: left; border-radius: 50%; margin-right: 25px" alt="Profile photo">
+                        <h2 class="col-md-4">{{$user->name}}</h2>
+
+                        {!! Form::open(['url' => url('/profile') ,'method' =>'post' , 'files' => true]) !!}
+
+                        <div class="form-group{{ $errors->has('avatar') ? ' has-error' : '' }}">
+                            <div class="col-md-6">
+                                {!! Form::file('avatar' , null , ['class' => "form-control"]) !!}
+
+                                @if ($errors->has('avatar'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('avatar') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-1 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    submit
+                                </button>
+                            </div>
+                        </div>
+
+
+                        {!! Form::close() !!}
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
