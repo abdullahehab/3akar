9197484b0f00f92606cbfbc35ab9b3690b2eda75
abdullah ehab <220150134@fa-hists.edu.eg>
2018-09-04 23:47:01 +0200

    add quick view of product

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,14 +6,27 @@
 
 @endsection
 
+@section('header')
+
+    {{ Html::style('home/css/reset.css') }}
+    {{ Html::style('home/css/style.css') }}
+    {{ Html::script('home/js/modernizr.js') }}
+
+{{--
+    <link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
+    <link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
+    <script src="js/modernizr.js"></script> <!-- Modernizr -->
+--}}
+
+@endsection
 
 @section("content")
     <div class="banner text-center">
         <div class="container">
             <div class="banner-info">
                 <h1> Welcome to {{ getSetting() }}</h1>
-                <p>
-                    {!! Form::open(['url' => 'search', 'method' => 'get']) !!}
+
+                {!! Form::open(['url' => 'search', 'method' => 'get']) !!}
                         <div class="row">
                             <div class="col-lg-3">
                                 {!! Form::text('bu_price_from',null, ['class' => 'form-control', 'placeholder' => 'Price From']) !!}
@@ -29,7 +42,7 @@
                                 {!! Form::select('bu_type',buType(),null, ['class' => 'form-control', 'placeholder' => 'Type of Build']) !!}
                             </div>
                         </div>
-                <br>
+                     <br>
                         <div class="row">
                             <div class="col-lg-3">
                                 {!! Form::submit('search', ['class' => 'btn', 'style' => 'width:100%']) !!}
@@ -47,150 +60,111 @@
                         </div>
                 {!! form::close() !!}
             </div>
-                </p>
-                <a class="banner_btn" href="{{ url('/showAllBuilding') }}">More</a> </div>
+                <a class="banner_btn" href="{{ url('/showAllBuilding') }}">More</a>
         </div>
     </div>
+
+
     <div class="main">
-        <div class="content_white">
-            <h2>Featured Services</h2>
-            <p>Quisque cursus metus vitae pharetra auctor, sem massa mattis semat interdum magna.</p>
-        </div>
-        <div class="featured_content" id="feature">
-            <div class="container">
-                <div class="row text-center">
-                    <div class="col-mg-3 col-xs-3 feature_grid1"> <i class="fa fa-cog fa-3x"></i>
-                        <h3 class="m_1"><a href="services.html">Legimus graecis</a></h3>
-                        <p class="m_2">Lorem ipsum dolor sit amet, facilisis egestas sodales non luctus, sem quas potenti malesuada vel phasellus.</p>
-                        <a href="services.html" class="feature_btn">More</a> </div>
-                    <div class="col-mg-3 col-xs-3 feature_grid1"> <i class="fa fa-comments-o fa-3x"></i>
-                        <h3 class="m_1"><a href="services.html">Mazim minimum</a></h3>
-                        <p class="m_2">Lorem ipsum dolor sit amet, facilisis egestas sodales non luctus, sem quas potenti malesuada vel phasellus.</p>
-                        <a href="services.html" class="feature_btn">More</a> </div>
-                    <div class="col-md-3 col-xs-3 feature_grid1"> <i class="fa fa-globe fa-3x"></i>
-                        <h3 class="m_1"><a href="services.html">Modus altera</a></h3>
-                        <p class="m_2">Lorem ipsum dolor sit amet, facilisis egestas sodales non luctus, sem quas potenti malesuada vel phasellus.</p>
-                        <a href="services.html" class="feature_btn">More</a> </div>
-                    <div class="col-md-3 col-xs-3 feature_grid2"> <i class="fa fa-history fa-3x"></i>
-                        <h3 class="m_1"><a href="services.html">Melius eligendi</a></h3>
-                        <p class="m_2">Lorem ipsum dolor sit amet, facilisis egestas sodales non luctus, sem quas potenti malesuada vel phasellus.</p>
-                        <a href="services.html" class="feature_btn">More</a> </div>
-                </div>
-            </div>
-        </div>
-        <div class="about-info">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8">
-                        <div class="block-heading-two">
-                            <h2><span>About Our Company</span></h2>
-                        </div>
-                        <p>Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero.</p>
-                        <br>
-                        <p>Sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-                        <a class="banner_btn" href="about.html">Read More</a> </div>
-                    <div class="col-md-4">
-                        <div class="block-heading-two">
-                            <h3><span>Our Advantages</span></h3>
-                        </div>
-                        <div class="panel-group" id="accordion-alt3">
-                            <div class="panel">
-                                <div class="panel-heading">
-                                    <h4 class="panel-title"> <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseOne-alt3"> <i class="fa fa-angle-right"></i> Quisque cursus metus vitae pharetra auctor</a> </h4>
-                                </div>
-                                <div id="collapseOne-alt3" class="panel-collapse collapse">
-                                    <div class="panel-body">
-                                        <p>Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="panel">
-                                <div class="panel-heading">
-                                    <h4 class="panel-title"> <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseTwo-alt3"> <i class="fa fa-angle-right"></i> Duis autem vel eum iriure dolor in hendrerit</a> </h4>
-                                </div>
-                                <div id="collapseTwo-alt3" class="panel-collapse collapse">
-                                    <div class="panel-body">
-                                        <p>Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="panel">
-                                <div class="panel-heading">
-                                    <h4 class="panel-title"> <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseThree-alt3"> <i class="fa fa-angle-right"></i> Quisque cursus metus vitae pharetra auctor </a> </h4>
-                                </div>
-                                <div id="collapseThree-alt3" class="panel-collapse collapse">
-                                    <div class="panel-body">
-                                        <p>Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="panel">
-                                <div class="panel-heading">
-                                    <h4 class="panel-title"> <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseFour-alt3"> <i class="fa fa-angle-right"></i> Duis autem vel eum iriure dolor in hendrerit</a> </a> </h4>
-                                </div>
-                                <div id="collapseFour-alt3" class="panel-collapse collapse">
-                                    <div class="panel-body">
-                                        <p>Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="highlight-info">
-            <div class="overlay spacer">
-                <div class="container">
-                    <div class="row text-center">
-                        <div class="col-sm-3 col-xs-6"> <i class="fa fa-smile-o fa-5x"></i>
-                            <h4>120+ Happy Clients</h4>
-                        </div>
-                        <div class="col-sm-3 col-xs-6"> <i class="fa fa-check-square-o fa-5x"></i>
-                            <h4>600+ Projects Completed</h4>
-                        </div>
-                        <div class="col-sm-3 col-xs-6"> <i class="fa fa-trophy fa-5x"></i>
-                            <h4>25 Awards Won</h4>
-                        </div>
-                        <div class="col-sm-3 col-xs-6"> <i class="fa fa-map-marker fa-5x"></i>
-                            <h4>3 Offices</h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="testimonial-area">
-            <div class="testimonial-solid">
-                <div class="container">
-                    <h2>Client Testimonials</h2>
-                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"> <a href="#"></a> </li>
-                            <li data-target="#carousel-example-generic" data-slide-to="1" class=""> <a href="#"></a> </li>
-                            <li data-target="#carousel-example-generic" data-slide-to="2" class=""> <a href="#"></a> </li>
-                            <li data-target="#carousel-example-generic" data-slide-to="3" class=""> <a href="#"></a> </li>
-                        </ol>
-                        <div class="carousel-inner">
-                            <div class="item active">
-                                <p>"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam quis nostrud exerci tation."</p>
-                                <p><strong>- John Doe -</strong></p>
-                            </div>
-                            <div class="item">
-                                <p>"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam quis nostrud exerci tation."</p>
-                                <p><strong>- Jane Doe -</strong></p>
-                            </div>
-                            <div class="item">
-                                <p>"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam quis nostrud exerci tation."</p>
-                                <p><strong>- John Smith -</strong></p>
-                            </div>
-                            <div class="item">
-                                <p>"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam quis nostrud exerci tation."</p>
-                                <p><strong>- Linda Smith -</strong></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+
+        <ul class="cd-items cd-container">
+            <li class="cd-item">
+                <img src="home/img/item-1.jpg" alt="Item Preview">
+                <a href="#0" class="cd-trigger">Quick View</a>
+            </li> <!-- cd-item -->
+
+            <li class="cd-item">
+                <img src="home/img/item-1.jpg" alt="Item Preview">
+                <a href="#0" class="cd-trigger">Quick View</a>
+            </li> <!-- cd-item -->
+
+            <li class="cd-item">
+                <img src="home/img/item-1.jpg" alt="Item Preview">
+                <a href="#0" class="cd-trigger">Quick View</a>
+            </li> <!-- cd-item -->
+
+            <li class="cd-item">
+                <img src="home/img/item-1.jpg" alt="Item Preview">
+                <a href="#0" class="cd-trigger">Quick View</a>
+            </li> <!-- cd-item -->
+
+            <li class="cd-item">
+                <img src="home/img/item-1.jpg" alt="Item Preview">
+                <a href="#0" class="cd-trigger">Quick View</a>
+            </li> <!-- cd-item -->
+
+            <li class="cd-item">
+                <img src="home/img/item-1.jpg" alt="Item Preview">
+                <a href="#0" class="cd-trigger">Quick View</a>
+            </li> <!-- cd-item -->
+
+            <li class="cd-item">
+                <img src="home/img/item-1.jpg" alt="Item Preview">
+                <a href="#0" class="cd-trigger">Quick View</a>
+            </li> <!-- cd-item -->
+
+            <li class="cd-item">
+                <img src="home/img/item-1.jpg" alt="Item Preview">
+                <a href="#0" class="cd-trigger">Quick View</a>
+            </li> <!-- cd-item -->
+
+            <li class="cd-item">
+                <img src="home/img/item-1.jpg" alt="Item Preview">
+                <a href="#0" class="cd-trigger">Quick View</a>
+            </li> <!-- cd-item -->
+
+            <li class="cd-item">
+                <img src="home/img/item-1.jpg" alt="Item Preview">
+                <a href="#0" class="cd-trigger">Quick View</a>
+            </li> <!-- cd-item -->
+
+            <li class="cd-item">
+                <img src="home/img/item-1.jpg" alt="Item Preview">
+                <a href="#0" class="cd-trigger">Quick View</a>
+            </li> <!-- cd-item -->
+
+            <li class="cd-item">
+                <img src="home/img/item-1.jpg" alt="Item Preview">
+                <a href="#0" class="cd-trigger">Quick View</a>
+            </li> <!-- cd-item -->
+        </ul> <!-- cd-items -->
+
+        <div class="cd-quick-view">
+            <div class="cd-slider-wrapper">
+                <ul class="cd-slider">
+                    <li class="selected"><img src="home/img/item-1.jpg" alt="Product 1"></li>
+                    <li><img src="home/img/item-2.jpg" alt="Product 2"></li>
+                    <li><img src="home/img/item-3.jpg" alt="Product 3"></li>
+                </ul> <!-- cd-slider -->
+
+                <ul class="cd-slider-navigation">
+                    <li><a class="cd-next" href="#0">Prev</a></li>
+                    <li><a class="cd-prev" href="#0">Next</a></li>
+                </ul> <!-- cd-slider-navigation -->
+            </div> <!-- cd-slider-wrapper -->
+
+            <div class="cd-item-info">
+                <h2>Produt Title</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, omnis illo iste ratione. Numquam eveniet quo, ullam itaque expedita impedit. Eveniet, asperiores amet iste repellendus similique reiciendis, maxime laborum praesentium.</p>
+
+                <ul class="cd-item-action">
+                    <li><button class="add-to-cart">Add to cart</button></li>
+                    <li><a href="#0">Learn more</a></li>
+                </ul> <!-- cd-item-action -->
+            </div> <!-- cd-item-info -->
+            <a href="#0" class="cd-close">Close</a>
+        </div> <!-- cd-quick-view -->
+
     </div>
+@endsection
+
+@section('footer')
+
+    {{ Html::script('home/js/jquery-2.1.1.js') }}
+    {{ Html::script('home/js/velocity.min.js') }}
+    {{ Html::script('home/js/main.js') }}
+{{--
+    <script src="{{ Request::root() }}/home/js/jquery-2.1.1.js"></script>
+    <script src="js/velocity.min.js"></script>
+    <script src="js/main.js"></script> <!-- Resource jQuery -->--}}
 @endsection
